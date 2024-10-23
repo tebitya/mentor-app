5895b40e0d5b83e93b234dd326c951c20093d214
Ian Knight <ian.knight.1990@gmail.com>
2024-10-23 14:24:46 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "uk.ac.nott.cs.comp2013"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
