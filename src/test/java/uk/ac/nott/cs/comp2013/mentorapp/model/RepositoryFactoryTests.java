c87d76d517ab1f38454bc7aa1d8777a051c55468
Ian Knight <ian.knight2@nottingham.ac.uk>
2024-11-16 19:00:40 +0000

    Add tests to verify implementation. Fixes #2

--- src/test/java/uk/ac/nott/cs/comp2013/mentorapp/model/RepositoryFactoryTests.java
+++ src/test/java/uk/ac/nott/cs/comp2013/mentorapp/model/RepositoryFactoryTests.java
@@ -1,43 +1,49 @@
 package uk.ac.nott.cs.comp2013.mentorapp.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.IOException;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.nott.cs.comp2013.mentorapp.model.user.Mentee;
 import uk.ac.nott.cs.comp2013.mentorapp.model.user.User;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RepositoryFactoryTests {
 
-  private RepositoryFactory factory;
+    private RepositoryFactory factory;
 
-  @BeforeEach
-  public void setUp() {
-    factory = new RepositoryFactory();
-  }
+    @BeforeEach
+    public void setUp() {
+        factory = new RepositoryFactory();
+    }
 
-  @Test
-  public void testUserRepositoryCsvMissing() {
-    assertThrows(IOException.class, () -> factory.userHashMapRepository("/MISSING_DATA.csv"),
-        "Missing data should have caused an exception");
-  }
+    @Test
+    public void testUserRepositoryCsvMissing() {
+        assertThrows(IOException.class, () -> factory.userHashMapRepository("/MISSING_DATA.csv"),
+                "Missing data should have caused an exception");
+    }
 
-  @Test
-  public void testUserRepositoryCsvPresent() throws IOException {
-    Repository<User, String> repo = factory.userHashMapRepository("/TEST_DATA.csv");
-    assertNotNull(repo, "Repository should have been created");
+    @Test
+    public void testUserRepositoryCsvPresent() throws IOException {
+        Repository<User, String> repo = factory.userHashMapRepository("/TEST_DATA.csv");
+        assertNotNull(repo, "Repository should have been created");
 
-    Optional<User> user = repo.selectById("labarough0");
-    assertTrue(user.isPresent(), "User should have been found");
-    assertEquals("labarough0", user.get().getUsername(), "Username should be labarough0");
-    assertEquals("gC9!dK+z!Fq$", user.get().getPassword(), "Password should be gC9!dK+z!Fq$");
-    assertInstanceOf(Mentee.class, user.get(), "User should be a Mentee");
-  }
+        Optional<User> user = repo.selectById("labarough0");
+        assertTrue(user.isPresent(), "User should have been found");
+        assertEquals("labarough0", user.get().getUsername(), "Username should be labarough0");
+        assertEquals("gC9!dK+z!Fq$", user.get().getPassword(), "Password should be gC9!dK+z!Fq$");
+        assertInstanceOf(Mentee.class, user.get(), "User should be a Mentee");
+    }
+
+    @Test
+    public void testUserRepositoryPasswordComma() throws IOException {
+        Repository<User, String> repo = factory.userHashMapRepository("/TEST_DATA.csv");
+
+        Optional<User> userOpt = repo.selectById("ahammerbergern8");
+        assertTrue(userOpt.isPresent(), "User should have been found");
+        assertEquals("oT5/YCHJ/u,g", userOpt.get().getPassword(), "Password was parsed incorrectly");
+    }
 
 }
