1b05462d2791dd5a206d599ede2042164fc94cbe
psytb12 <psytb12@nottingham.ac.uk>
2024-11-30 21:09:58 +0000

    Fixed role selection page, made changes to logincontroller

--- src/main/java/uk/ac/nott/cs/comp2013/mentorapp/controller/LoginController.java
+++ src/main/java/uk/ac/nott/cs/comp2013/mentorapp/controller/LoginController.java
@@ -3,13 +3,21 @@ package uk.ac.nott.cs.comp2013.mentorapp.controller;
 import java.util.Optional;
 import uk.ac.nott.cs.comp2013.mentorapp.model.Repository;
 import uk.ac.nott.cs.comp2013.mentorapp.model.user.User;
+import uk.ac.nott.cs.comp2013.mentorapp.model.user.UserRole;
 
 public class LoginController {
 
   private final Repository<User, String> repo;
+  private static UserRole role;
 
   public LoginController(Repository<User, String> model) {
     this.repo = model;
+  }
+
+  /* New getter to get the role of the user */
+  /* Used for the actorsveiw file */
+  public static UserRole getRole() {
+    return role;
   }
 
   public boolean onLoginClick(String username, String password) {
@@ -18,7 +26,13 @@ public class LoginController {
       return false;
     }
 
+
     User u = user.get();
-    return u.getUsername().equals(username) && u.getPassword().equals(password);
+    if(u.getUsername().equals(username) && u.getPassword().equals(password)){
+      /* Role recorded once current user logs in */
+      role = u.getRole();
+      return true;
+    }
+    return false;
   }
 }
--- src/main/java/uk/ac/nott/cs/comp2013/mentorapp/view/ActorsView.java
+++ src/main/java/uk/ac/nott/cs/comp2013/mentorapp/view/ActorsView.java
@@ -3,11 +3,14 @@ package uk.ac.nott.cs.comp2013.mentorapp.view;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import uk.ac.nott.cs.comp2013.mentorapp.controller.ActorsController;
+import uk.ac.nott.cs.comp2013.mentorapp.controller.LoginController;
+import uk.ac.nott.cs.comp2013.mentorapp.model.user.UserRole;
 
 public class ActorsView extends VBox implements ManagedView {
 
@@ -27,9 +30,58 @@ public class ActorsView extends VBox implements ManagedView {
         setSpacing(30);
         setStyle("-fx-background-color: #FDFBF9;");
 
-        Label titleLbl = new Label("Select your Role");
-        titleLbl.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
-        getChildren().add(titleLbl);
+        Label title = new Label("Select Your Role");
+        title.setStyle("-fx-text-fill: #10263B; -fx-font-size: 24px; -fx-font-weight: bold;");
+        title.setAlignment(Pos.CENTER);
+        getChildren().add(title);
+
+        /* Buttons created to display roles */
+        Button mentorBtn = new Button("Mentor");
+        Button menteeBtn = new Button("Mentee");
+        Button adminBtn = new Button("Admin");
+
+        String buttonStyle = "-fx-font-size: 20px; -fx-background-color: #10263B; -fx-text-fill: white;";
+        mentorBtn.setStyle(buttonStyle);
+        menteeBtn.setStyle(buttonStyle);
+        adminBtn.setStyle(buttonStyle);
+
+        /* Ensuring buttons cover width of page */
+        mentorBtn.setMaxWidth(Double.MAX_VALUE);
+        menteeBtn.setMaxWidth(Double.MAX_VALUE);
+        adminBtn.setMaxWidth(Double.MAX_VALUE);
+
+        /* Determining the button's actions once pressed */
+        mentorBtn.setOnAction(e -> roleSelection(UserRole.MENTOR));
+        menteeBtn.setOnAction(e -> roleSelection(UserRole.MENTEE));
+        adminBtn.setOnAction(e -> roleSelection(UserRole.ADMIN));
+
+        getChildren().addAll(mentorBtn, menteeBtn, adminBtn);
+    }
+
+    /* Function to check which role a user has */
+    private void roleSelection(UserRole selectedRole) {
+        UserRole actualRole = LoginController.getRole();
+        var eh = onViewChange.get();
+
+        if (eh != null) {
+            /* Checking button clicked against actual role */
+            /* And based on this going to correct view */
+            if (selectedRole == actualRole) {
+                if (selectedRole == UserRole.ADMIN) {
+                    eh.handle(new ViewChangeEvent(ViewManager.ADMIN_VIEW));
+                } else if (selectedRole == UserRole.MENTOR) {
+                    eh.handle(new ViewChangeEvent(ViewManager.MENTOR_VIEW));
+                } else if (selectedRole == UserRole.MENTEE) {
+                    eh.handle(new ViewChangeEvent(ViewManager.MENTEE_VIEW));
+                }
+            } else {
+                /* New label for when incorrect role is selected */
+                Label accessDenied = new Label("Access denied. Incorrect role selected.");
+                /* Similar styling to Moodle error messages */
+                accessDenied.setStyle("-fx-background-color: #EFCED0; -fx-text-fill: #682134; -fx-font-size: 14px;");
+                getChildren().add(accessDenied);
+            }
+        }
     }
 
     @Override
