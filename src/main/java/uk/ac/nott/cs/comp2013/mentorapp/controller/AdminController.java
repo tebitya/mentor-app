c62fe5f3beb3a41d2b0c0cef4021d3f266931161
psytb12 <psytb12@nottingham.ac.uk>
2024-12-04 16:29:24 +0000

    Refined list mentors function to ensure all mentors have at least 6 months for maximised mentorship availability!

--- src/main/java/uk/ac/nott/cs/comp2013/mentorapp/controller/AdminController.java
+++ src/main/java/uk/ac/nott/cs/comp2013/mentorapp/controller/AdminController.java
@@ -1,15 +1,20 @@
 package uk.ac.nott.cs.comp2013.mentorapp.controller;
 
+import javafx.util.Pair;
 import uk.ac.nott.cs.comp2013.mentorapp.model.Repository;
+import uk.ac.nott.cs.comp2013.mentorapp.model.user.HasAvailability;
 import uk.ac.nott.cs.comp2013.mentorapp.model.user.User;
 import uk.ac.nott.cs.comp2013.mentorapp.model.user.UserRole;
 
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDateTime;
 
 public class AdminController {
 
     private final Repository<User, String> userRepo;
+    private final List<Pair<String, String>> selectedPairs = new ArrayList<>();
 
     /* Using same params as login */
     public AdminController(Repository<User, String> model) {
@@ -34,13 +39,28 @@ public class AdminController {
     public List<String> listAllMentors(){
         List<String> mentorList = new ArrayList<>();
         /* Selecting all users where the role is set to mentor */
+        /* However, the data set is outdated and therefore main condition mentors
+        will be listed for is availability of minimum 6 months */
+
         for (User user : userRepo.selectAll()){
             if (user.getRole() == UserRole.MENTOR){
-                /* Taking only their username for now to display in combo box */
-                mentorList.add(user.getUsername());
+                if (user instanceof HasAvailability availability){
+                    LocalDateTime start = availability.getStartAvailability();
+                    LocalDateTime end = availability.getEndAvailability();
+
+                    /* Checking mentor has at least 6 months */
+                    /* Also ensures that each mentor has dates filled in for both start and end */
+                    if (start != null && end != null && ChronoUnit.MONTHS.between(start, end) >= 6){
+                        mentorList.add(user.getUsername());
+                    }
+                }
             }
         }
         return mentorList;
     }
+
+    /* Now move onto specifics in use case */
+    /* Adding the pair created */
+
 
 }
